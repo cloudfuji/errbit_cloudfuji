379d14cff35e1ce0edbf2106bbc9fd4b1756df77
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-29 20:33:43 +1200

    devise routes already loaded

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   begin
     cloudfuji_routes
-
-    if Cloudfuji::Platform.on_cloudfuji?
-      # Setup cloudfuji authentication routes
-      devise_cloudfuji_authenticatable
-    end
   rescue => e
     puts "Error loading the Cloudfuji routes:"
     puts "#{e.inspect}"
