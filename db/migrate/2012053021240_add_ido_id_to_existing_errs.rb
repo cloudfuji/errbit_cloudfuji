c3c46c1b19fdddbaff1b1702749d8041d24c1429
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-31 10:17:17 +1200

    Fixed migration and bumped timestamp to run it again

--- db/migrate/2012053021240_add_ido_id_to_existing_errs.rb
+++ db/migrate/2012053021240_add_ido_id_to_existing_errs.rb
@@ -1,6 +1,8 @@
 class AddIdoIdToExistingErrs < Mongoid::Migration
   def self.up
-    Err.where(:ido_id => nil).all do |err|
+    errs = Err.where(:ido_id => nil).all
+    puts "Generating 'ido_id's for #{errs.size} Errs..."
+    errs.each do |err|
       err.update_attribute :ido_id, UUID.new.generate
     end
   end
